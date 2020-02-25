d38e7b3c59dfe56693199a05f223404442456c7b
Dan Brumleve <jdb1729@gmail.com>
2020-02-25 17:44:45 -0600

    common encoding options

--- vidwriter.pl
+++ vidwriter.pl
@@ -8,12 +8,16 @@ if (my $filter = $ENV{TEWEL_VIDWRITER_FILTER}) {
 }
 
 my $rate = $ENV{TEWEL_VIDWRITER_RATE} || 16;
+my $bitrate = $ENV{TEWEL_VIDWRITER_BITRATE};
+my $codec = $ENV{TEWEL_VIDWRITER_CODEC};
 
 exec('ffmpeg',
   '-hide_banner', '-loglevel', 'quiet', '-nostats', '-nostdin',
   '-r', $rate,
   '-i', '/dev/stdin',
   '-r', $rate,
+  $bitrate ? ('-b:v', $bitrate) : ( ),
+  $codec ? ('-vcodec', $codec) : ( ),
   '-y', $fn
 );
 die "$0: ffmpeg: $!\n";
