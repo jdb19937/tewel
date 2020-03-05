02a4af414c5607e7627450fd62d7753f885ab70d
Dan Brumleve <dan@makemoresoftware.com>
2020-03-05 00:55:25 -0600

    don't change stochastic params when noramlizing

--- cortex.cc
+++ cortex.cc
@@ -1331,18 +1331,6 @@ void Cortex::_put_head_op(const double *vec, const double *mat, int w, int h) {
       wmv[3 * (voc + x + y * w)] = mat[x + y * w];
   for (int y = 0; y < h; ++y)
     wmv[3 * y] = vec[y];
-
-  {
-    for (int y = 0; y < h; ++y)
-      for (int x = 0; x < w; ++x) {
-        wmv[3 * (voc + x + y * w) + 1] = 0;
-        wmv[3 * (voc + x + y * w) + 2] = 1024.0;
-      }
-    for (int y = 0; y < h; ++y) {
-      wmv[3 * y + 1] = 0;
-      wmv[3 * y + 2] = 1024.0;
-    }
-  }
 }
 
 void Cortex::_get_tail_op(double **vecp, double **matp, int *wp, int *hp) {
@@ -1411,18 +1399,6 @@ void Cortex::_put_tail_op(const double *vec, const double *mat, int w, int h) {
           wmv[3 * (voc + x + y * w)] = mat[x + y * w];
       for (int y = 0; y < h; ++y)
         wmv[3 * y] = vec[y];
-
-      {
-        for (int y = 0; y < h; ++y)
-          for (int x = 0; x < w; ++x) {
-            wmv[3 * (voc + x + y * w) + 1] = 0;
-            wmv[3 * (voc + x + y * w) + 2] = 1024.0;
-          }
-        for (int y = 0; y < h; ++y) {
-          wmv[3 * y + 1] = 0;
-          wmv[3 * y + 2] = 1024.0;
-        }
-      }
 
       return;
     }
